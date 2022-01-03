c5afb923020946dde5d4c767852fc50c259d0245
Eugene <evgenyk5@rambler.ru>
2022-01-03 14:05:31 +0300

    index.php 1.1.1

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 printf("Hello world. server1<br><br>");
-printf("by version: 1.1<br><br>");
+printf("by version: 1.1.1<br><br>");
 printf("Deployed by ConcourseCI<br><br>");
 echo $_SERVER['SERVER_ADDR'];
 
