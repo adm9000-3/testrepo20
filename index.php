d28275958126013fbec44b7829891644f1f6d2bf
Eugene <evgenyk5@rambler.ru>
2022-01-03 13:46:44 +0300

    index.php modified

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
-printf("Hello world. server1  servers<br><br>");
-printf("by version: 1.0<br><br>");
-
+printf("Hello world. server1<br><br>");
+printf("by version: 1.1<br><br>");
+printf("Deployed by ConcourseCI<br><br>");
 echo $_SERVER['SERVER_ADDR'];
 
 ?>
